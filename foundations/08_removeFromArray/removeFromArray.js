4ff8a42757d3b9ff729bd4f8ccb00220e96b0178
Katalout <253144538+Katalout@users.noreply.github.com>
2026-02-11 18:36:36 +0100

    Create function that removes items from array, use "arguments"
    First exercise to deal with undeclared function parameters,
    hella interesting and I nailed it muhahah.
    
    modified:   foundations/08_removeFromArray/removeFromArray.js
    modified:   foundations/08_removeFromArray/removeFromArray.spec.js

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, item) {
+    for (i = 1; i < arguments.length; i++) {
+        if (!(array.includes(arguments[i]))) continue;
+        array.splice(array.indexOf(arguments[i]), 1);
+        if (array.includes(arguments[i])) i--;;
+    }
+    return array;
 };
 
 // Do not edit below this line
