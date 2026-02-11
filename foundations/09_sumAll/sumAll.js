3c43f8b2f9a4b432b1316b1bc36bf582cb99c592
Katalout <253144538+Katalout@users.noreply.github.com>
2026-02-11 20:26:35 +0100

    Create function to sum ints between two parameters
    Interesting lesson that sneaky includes destructuring,
    but it was possible to solve without.
    
    	modified:   foundations/09_sumAll/sumAll.js
    	modified:   foundations/09_sumAll/sumAll.spec.js

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (
+        a < 0 ||
+        b < 0 ||
+        Math.floor(a) !== a ||
+        Math.floor(b) !== b ||
+        (typeof a) !== "number" ||
+        (typeof b) !== "number"
+    ) return "ERROR";
+    let sum = 0;
+    if (a > b) {
+        for (i = b; i <= a; i++) {
+            sum = sum + i;
+        }
+    }
+    for (i = a; i <= b; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
