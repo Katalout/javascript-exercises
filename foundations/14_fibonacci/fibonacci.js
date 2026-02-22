d43467fbc58975bd067825f57dd054ed46177908
Katalout <253144538+Katalout@users.noreply.github.com>
2026-02-22 19:22:37 +0100

    Create function to calculate fibonacci
    	modified:   fibonacci.js
    	modified:   fibonacci.spec.js

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (num) {
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+    let x = 0;
+    let y = 1;
+    let result;
+    for (i = 0; i < +num; i++) {
+        console.log(`x=${x}, y=${y}`);
+        result = x + y;
+        y = x;
+        x = result;
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
