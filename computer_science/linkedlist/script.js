0bee43bd0d37e54996bd526d38039c455e3f1190
Katalout <253144538+Katalout@users.noreply.github.com>
2026-06-18 16:18:21 +0200

    Add methods to linkedlist

--- computer_science/linkedlist/script.js
+++ computer_science/linkedlist/script.js
@@ -1,30 +1,93 @@
-class LinkedList {
-    constructor(head, tail) {
-        this.head = head;
-        this.tail = tail;
-        this.length = 0;
+class linkedList {
+    constructor() {
+        this._head = null;
+        this._tail = null;
+        this._size = 0;
     }
-    append(value) {
-        let node = new Node(value);
-        //current tail's nextNode to be updated to this one
-        //list's tail to be reassigned to this one
-        this.length++;
+    prepend(value, next = this._head) {
+        let node = new Node(value, next);
+        this._head = node;
+        if (this._size === 0) this._tail = node;
+        this._size++;
     }
 
-    prepend(value, next) {
-        let node = new Node(value, next);
-        //next is the previous head
-        //list's head to be reassigned to this one
-        this.length++;
+    append(value) {
+        let node = new Node(value);
+        //current tail's next to be updated to this one
+        (this._size === 0) ? this._head = node : this._tail.next = node;
+        this._tail = node;
+        this._size++;
     }
-    size() {
-        return this.length;
+
+    get size() {
+        return this._size;
+    }
+    get head() {
+        return this._head ? this._head.value : undefined;
+    }
+    get tail() {
+        return this._tail ? this._tail.value : undefined;
+    }
+    at(index) {
+        if (this._size === 0) return undefined;
+        if (index === this._size - 1) return this._tail.value;
+        let current = this._head;
+        let i = 0;
+        while (i < this._size) {
+            if (i === index) return current.value;
+            current = current.next;
+            i++;
+        }
+    }
+    pop() {
+        if (this._size === 0) return undefined;
+        let head = this._head;
+        this._head = this._head.next;
+        if (this._size === 1) this._tail = null;
+        head.next = null;
+        this._size--;
+        return head.value;
+    }
+    contains(value) {
+        return this.iterator(value, true, false);
+    }
+
+    findIndex(value) {
+        return this.iterator(value, "index", -1);
+    }
+    toString() {
+        let string = "";
+        let current = this._head;
+        let i = 0;
+        while (i < this._size) {
+            string += `( ${current.value} ) -> `;
+            if (i === this._size - 1) string += "null";
+            current = current.next;
+            i++;
+            //this should also be iteratored but i can't be arsed to tweak it
+        }
+        console.log(string);
+    }
+    iterator(value, success, fail) {
+        let current = this._head;
+        let i = 0;
+        while (i < this._size) {
+            if (value === current.value) return success === "index" ? i : success;
+            current = current.next;
+            i++;
+        }
+        return fail;
     }
 }
 
 class Node {
     constructor(value = null, next = null) {
         this.value = value;
-        this.nextNode = next;
+        this.next = next;
     }
 }
+
+const list = new linkedList;
+list.prepend("origi head");
+list.append("origi tail");
+console.log(list);
