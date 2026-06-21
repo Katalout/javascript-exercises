d784800129329212f3f7425dceb6e04764c3adc2
Katalout <253144538+Katalout@users.noreply.github.com>
2026-06-21 18:09:06 +0200

    Add growcapacity function

--- computer_science/hashmap/script.js
+++ computer_science/hashmap/script.js
@@ -1,10 +1,17 @@
-class HashMap {
+export class HashMap {
     constructor(loadfactor = 0.75, capacity = 16) {
         this.loadfactor = loadfactor;
         this.capacity = capacity;
-        this.arrayOfBuckets = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []];
+        this._length = 0;
+        this.arrayOfBuckets = Array(this.capacity).fill(undefined).map(x => x = []);
     }
-
+    get loadlevel() {
+        return this._length / this.capacity;
+    }
+    get isOverLimit() {
+        if (this.loadlevel > this.loadfactor) return true;
+        else return false;
+    }
     hash(key) {
         let hashCode = 0;
         const primeNumber = 31;
@@ -23,6 +30,15 @@ class HashMap {
             }
         }
         bucket.push({ key, value });
+        this._length++;
+        if (this.isOverLimit) this.grow();
+    }
+    grow() {
+        let entries = this.entries();
+        this.clear();
+        this.capacity *= 2;
+        this.arrayOfBuckets = Array(this.capacity).fill(undefined).map(x => x = []);
+        entries.forEach(entry => this.set(entry.key, entry.value));
     }
     get(key) {
         let bucket = this.bucket(key);
@@ -51,12 +67,36 @@ class HashMap {
         let index = bucket.findIndex(entry => entry.key === key);
         if (index >= 0) {
             bucket.splice(index, 1);
+            this._length--;
             return true;
         }
         return false;
     }
+    get length() { return this._length }
+    clear() {
+        this.arrayOfBuckets.forEach(bucket => bucket.splice(0));
+        this._length = 0;
+    }
+    keys() {
+        let array = [];
+        this.arrayOfBuckets.forEach(bucket => {
+            bucket.forEach(entry => array.push(entry.key))
+        });
+        return array;
+    }
+    values() {
+        let array = [];
+        this.arrayOfBuckets.forEach(bucket => {
+            bucket.forEach(entry => array.push(entry.value))
+        });
+        return array;
+    }
+    entries() {
+        let array = [];
+        this.arrayOfBuckets.forEach(bucket => {
+            bucket.forEach(entry => array.push(entry))
+        });
+        return array;
+    }
 }
 
-const hashmap = new HashMap();
-hashmap.set("emez", "amaz");
-console.log(hashmap);
